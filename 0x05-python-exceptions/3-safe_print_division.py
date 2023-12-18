34015d63857a82d0320696909ba01626b8a38c93
root <gitongasamuel368@gmail.com>
2023-12-18 06:51:23 -0800

    except error

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -8,7 +8,7 @@ def safe_print_division(a, b):
     """
     try:
         res = a / b
-    except IndexError:
+    except:
         res = None
     finally:
         print("Inside result: {}".format(res))
