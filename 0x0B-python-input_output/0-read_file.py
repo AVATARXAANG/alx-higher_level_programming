bdde2a5b3a0295e4266097aae2ce99b7a049995b
root <gitongasamuel368@gmail.com>
2024-01-13 03:00:44 -0800

    hot fix

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,5 +2,6 @@
 def read_file(filename=""):
     """ function that reads a text file (UTF8) and prints it to stdout """
 
-    with open(filename, encoding='utf-8') as f:
-        print(f.read(), end="")
+    with open(filename, encoding='utf-8') as file:
+        lines=file.read()
+        print(lines)
